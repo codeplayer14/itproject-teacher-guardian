6fd025f5adc1713448dde863d53bd5eb62a25b7a
Padmanabh Gupta <padmanabh14@gmail.com>
2018-11-06 17:54:48 +0530

    Added admin pages

---
+++ TeacherGuardian/AcceptRequests.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AcceptRequests.aspx.cs" Inherits="AcceptRequests" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+     <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="Data Source=(localdb)\mssqllocaldb;Initial Catalog=TGdatabase;Integrated Security=True" DeleteCommandType="StoredProcedure" ProviderName="System.Data.SqlClient" SelectCommand="SELECT RequestId,StudentName,StudentId,FacultyName,FacultyId from Request"></asp:SqlDataSource>
+    <asp:GridView ID="gridview1" runat="server" AutoGenerateSelectButton="True" DataSourceID="SqlDataSource1" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+    </asp:GridView>
+    <asp:Button ID="AcceptButton" runat="server" OnClick="AcceptSelected" Text="Accept Selected" />
+
+    <asp:Button ID="DeclineButton" runat="server" OnClick= "DeclineSelected" Text="Decline Selected" />
+ 
+</asp:Content>
+
---
+++ TeacherGuardian/AssignGuardians.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AssignGuardians : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void AssignGuardian(object sender, EventArgs e)
+    {
+        string StudId = ListBox2.SelectedItem.Value;
+        string FacId = ListBox1.SelectedItem.Value;
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=TeacherGuardian;Integrated Security=True;";
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Student SET FacultyId=@facid, HasGuardian=1 WHERE Id=@studid", con);
+            cmd.Parameters.AddWithValue("@facid", FacId);
+            cmd.Parameters.AddWithValue("@studid", StudId);
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+}
