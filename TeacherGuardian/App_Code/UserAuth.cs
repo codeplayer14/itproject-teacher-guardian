e02da7094289e7832c5a57a512db256071ec4557
Padmanabh Gupta <padmanabh14@gmail.com>
2018-11-07 04:13:12 +0530

    Added authentication class

--- TeacherGuardian/App_Code/UserAuth.cs
+++ TeacherGuardian/App_Code/UserAuth.cs
@@ -6,12 +6,40 @@ using System.Web;
 /// <summary>
 /// Summary description for UserAuth
 /// </summary>
-public class UserAuth
+public static class UserAuth
 {
-    public UserAuth()
+
+    public static void AuthenticateUser(string loginPage,Boolean isAdmin)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        HttpCookie cookie = HttpContext.Current.Request.Cookies["user"];
+        if (cookie == null)
+        {
+            HttpContext.Current.Response.Redirect(loginPage);
+            //Redirect
+        }
+
+        if (Convert.ToBoolean(cookie["authenticated"].ToString()))
+        {
+            if (isAdmin == true)
+            {
+                if (cookie["admin"] == null)
+                {
+
+                    HttpContext.Current.Response.Redirect(loginPage);
+                }
+                if (!Convert.ToBoolean(cookie["admin"].ToString()))
+                {
+
+                    HttpContext.Current.Response.Redirect(loginPage);
+
+                }
+            }
+            return;
+        }
+        else
+        {
+            HttpContext.Current.Response.Redirect(loginPage);
+        }
+
     }
 }
