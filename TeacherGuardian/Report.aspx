a8ab551d210d55fcf13edaaaeb22405b2697d85e
Padmanabh Gupta <padmanabh14@gmail.com>
2018-11-07 05:58:10 +0530

    Core functionlaties working

--- TeacherGuardian/Report.aspx
+++ TeacherGuardian/Report.aspx
@@ -3,5 +3,31 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:SqlDataSource ID="sqlds" runat="server" ConnectionString="<%$ ConnectionStrings:con %>" SelectCommand="Select * from Guardian g  inner join student s on g.studentid = s.id"></asp:SqlDataSource>
+    
+    <asp:DropDownList ID="Category" runat="server">
+       <asp:ListItem Value="StudentName">Student Name</asp:ListItem>
+        
+       <asp:ListItem Value="Section">Section</asp:ListItem>
+        
+       <asp:ListItem Value="Branch">Stream</asp:ListItem>
+        
+       <asp:ListItem Value="FacultyName">Guardian Assigned</asp:ListItem>
+    </asp:DropDownList>
+    
+    <asp:Label runat="server" Text="Enter parameter value: "></asp:Label>
+    <asp:TextBox ID="categoryValue" runat="server"></asp:TextBox>
+    <asp:Button runat="server" ID="search" OnClick="search_Click" Text="Search"/>
+    <asp:Button  runat="server" ID="showAll" OnClick="showAll_Click" Text="Show All"/>
+    <br />
+    <br />
+<asp:GridView runat="server" AutoGenerateColumns="false" ID="gv" DataSourceID="sqlds">
+    <Columns>
+        <asp:BoundField  DataField="StudentName" HeaderText="Student Name"/>
+        <asp:BoundField DataField="Section" HeaderText="Section"/>
+        <asp:BoundField DataField="Branch" HeaderText="Stream" />
+        <asp:BoundField DataField="facultyname" HeaderText="Guardian Assigned" />
+    </Columns>  
+</asp:GridView>
 </asp:Content>
 
--- TeacherGuardian/Report.aspx.cs
+++ TeacherGuardian/Report.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,9 +10,56 @@ using System.Web.UI.WebControls;
 
 public partial class Report : System.Web.UI.Page
 {
+    DataSet ds;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
         UserAuth.AuthenticateUser("Admin.aspx",true);
+            
+        if (Request.QueryString["searchParam"] != null)
+        {
+            string searchParam = Request.QueryString["searchParam"].ToString();
+
+            string searchValue = Request.QueryString["searchValue"].ToString();
+
+            SqlConnection con = new SqlConnection(); ;
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+            try
+            {
+
+                con.Open();
+                System.Diagnostics.Debug.WriteLine("VALUES: " + searchParam + " " + searchValue);
+                
+                SqlDataAdapter sda = new SqlDataAdapter("Select * from Guardian g  inner join student s on g.studentid = s.id",con);
+                
+                ds = new DataSet();
+                sda.Fill(ds,"details");
+                ds.Tables[0].DefaultView.RowFilter = searchParam +"='"+ searchValue+"'";
+                gv.DataSourceID = null;
+                gv.DataSource = ds.Tables[0].DefaultView;
+                gv.DataBind();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+            finally
+            {
+                con.Close();
+
+            }
+        }
+    }
+
+    protected void search_Click(object sender, EventArgs e)
+    {
+        string queryString = "searchParam=" +Server.UrlEncode(Category.SelectedValue) + " &searchValue=" + Server.UrlEncode(categoryValue.Text);
+        Response.Redirect("Report.aspx?"+queryString);
+    }
+
+    protected void showAll_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Report.aspx");
     }
 }
